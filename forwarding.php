617853a90f97e51c48e179b2ec271544c606775d
Takuo Kihira <t-kihira@broadtail.jp>
2022-04-07 06:34:53 +0900

    Refactor

--- forwarding.php
+++ forwarding.php
@@ -1,3 +1,4 @@
 <?php
     $datetime = $_GET['datetime'];
+    header("Access-Control-Allow-Origin: *");
     echo file_get_contents("http://www.kmoni.bosai.go.jp/webservice/hypo/eew/".$datetime.".json?jsoncallback=callback");
